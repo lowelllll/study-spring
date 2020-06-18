6c6aeb37fa2df68c535dc603729ffda63169d6c0
lowelllll <yejin2651@naver.com>
2020-06-18 23:37:01 +0900

    using component scan

---
+++ SimpleMemberWebProject/src/main/java/com/lowell/member/service/IMemberService.java
@@ -0,0 +1,10 @@
+package com.lowell.member.service;
+
+import com.lowell.member.dto.Member;
+
+public interface IMemberService {
+	void memberRegister(String memId, String memPw, String memMail, String memPhone1, String memPhone2, String memPhone3);
+	Member memberSearch(String memId, String memPw);
+	void memberModify();
+	void memberRemove();
+}
