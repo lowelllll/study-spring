5b9b765ac1963359c68b8095f1e58dcd7b533865
lowelllll <yejin2651@naver.com>
2020-07-07 23:46:34 +0900

    use annotation ModelAttribute

--- SimpleMemberWebProject/src/main/java/com/lowell/member/controller/MemberController.java
+++ SimpleMemberWebProject/src/main/java/com/lowell/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +20,7 @@ public class MemberController {
 	MemberService service;
 	
 	@RequestMapping(value="/memJoin", method=RequestMethod.POST)
-	public String memJoin(Member member) {
+	public String memJoin(@ModelAttribute("mem") Member member) {
 		
 		service.memberRegister(member.getMemId(), member.getMemPw(), member.getMemMail(), 
 				member.getMemPhone1(), member.getMemPhone2(), member.getMemPhone3());
