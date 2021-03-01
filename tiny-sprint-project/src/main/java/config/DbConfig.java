2f3c2a51b16a647ba00a70994dd905bcc0b0267b
lowelllll <yejin2651@naver.com>
2021-03-01 23:31:41 +0900

    use @Transactional

--- tiny-sprint-project/src/main/java/config/DbConfig.java
+++ tiny-sprint-project/src/main/java/config/DbConfig.java
@@ -3,9 +3,13 @@ package config;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import spring.MemberDao;
 
 @Configuration
+@EnableTransactionManagement
 public class DbConfig {
 
     @Bean(destroyMethod = "close")
@@ -18,6 +22,13 @@ public class DbConfig {
         dataSource.setInitialSize(2);
         dataSource.setMaxActive(10);
         return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource());
+        return transactionManager;
     }
 
     @Bean
--- tiny-sprint-project/src/main/java/spring/ChangePasswordService.java
+++ tiny-sprint-project/src/main/java/spring/ChangePasswordService.java
@@ -2,6 +2,7 @@ package spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class ChangePasswordService {
@@ -9,6 +10,7 @@ public class ChangePasswordService {
     @Autowired
     private MemberDao memberDao;
 
+    @Transactional
     public void changePassword(String email, String oldPassword, String newPassword) {
         Member member = memberDao.selectByEmail(email);
         if (member == null)
